30949c0ee0a56d51f0912d91886fe02e26dc7e41
Zhuraveles <149628254+Zhuraveles@users.noreply.github.com>
2024-04-16 13:49:33 +0300

    FE-5

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     parallax:true,
-    speed:1000,
     spaceBetween: 16,
     centeredSlides: true,
     pagination: {
@@ -22,7 +21,30 @@ document.addEventListener("DOMContentLoaded", function () {
     scrollbar: {
       el: '.swiper-scrollbar',
     },*/
-
   });
-
 });
+
+
+let p = 0;
+let brandElem = document.getElementById('brands');
+let readMoreElem = brandElem.querySelector('.content__readMore');
+let text = brandElem.querySelector('.readMore__text');
+
+readMoreElem.onclick = function() {
+  p = p + 1;
+  brandElem.classList.toggle('brands_open');
+  if (p % 2 == 1) { text.textContent = "Скрыть"; }
+  else { text.textContent = "Показать все"; }
+};
+
+
+let wrapperElem = document.getElementById('wrapper');
+let buttonOpenElem = wrapperElem.querySelector('.buttonOpen');
+let buttonCLoseElem = wrapperElem.querySelector('.buttonCLose');
+
+buttonCLoseElem.onclick = function() {
+  wrapperElem.classList.toggle('withLeftMenu');
+};
+buttonOpenElem.onclick = function() {
+  wrapperElem.classList.toggle('withLeftMenu');
+};
